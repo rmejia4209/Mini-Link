746022cb56b0db764163ab9031efb86dcd4ef8c0
Rigo Mejia <rmejia4209@gmail.com>
2025-01-26 21:45:27 -0600

    Added unit test for valid domains

--- api_test.py
+++ api_test.py
@@ -4,7 +4,7 @@ from utils import get_url_pattern_match
 
 class RegexUnitTest(unittest.TestCase):
 
-    def test_valid_url_protocols(self):
+    def test_valid_url_protocols(self) -> None:
         """
         Test if the regex correcly matches URL's with the correct protocol.
         """
@@ -13,7 +13,7 @@ class RegexUnitTest(unittest.TestCase):
             with self.subTest(sample=sample):
                 self.assertEqual(get_url_pattern_match(sample), sample)
 
-    def test_invalid_url_protocols(self):
+    def test_invalid_url_protocols(self) -> None:
         """
         Test if the regex does not match URL's with incorrect protocols
         """
@@ -25,7 +25,10 @@ class RegexUnitTest(unittest.TestCase):
             with self.subTest(sample=sample):
                 self.assertFalse(get_url_pattern_match(sample))
 
-    def test_invalid_authority(self):
+    def test_invalid_authority(self) -> None:
+        """
+        Test if the regex does not match URL's with invalid authorities
+        """
 
         samples = [
             'https:///google.com', 'https://user:password@google.com',
@@ -35,6 +38,20 @@ class RegexUnitTest(unittest.TestCase):
         for sample in samples:
             with self.subTest(sample=sample):
                 self.assertFalse(get_url_pattern_match(sample))
+
+    def test_valid_domains(self) -> None:
+        """
+        Test if the regex correctly matches given URL domains
+        """
+
+        urls = [
+            'https://google.com', 'https://wikipedia.org',
+            'https://php.net', 'https://snake.io',
+            'https://oregonstate.edu', 'https://irs.gov'
+        ]
+        for url in urls:
+            with self.subTest(url=url):
+                self.assertEqual(get_url_pattern_match(url), url)
 
 
 if __name__ == '__main__':
