cea299ec1a8ceecf3d864791f92d77e2d7dbd6af
Rigo Mejia <rmejia4209@gmail.com>
2025-01-27 14:54:26 -0600

    API does not correcly reject urls with invalid authorities

--- api_test.py
+++ api_test.py
@@ -5,30 +5,45 @@ import unittest
 class RegexUnitTest(unittest.TestCase):
     base_url: str = 'http://localhost:8000/'
 
-    def get_mini_url(self, url: str) -> tuple[int, dict[str, str]]:
-
+    def get_mini_url(self, url: str) -> int:
+        """
+        Returns the API's resonse status code for a post request with
+        the given URL.
+        """
         res = requests.post(self.base_url, json={'url': url})
         return res.status_code
 
-    def test_valid_url_protocols(self) -> None:
-        urls = ['https://github.com', 'http://google.com']
-
-        expected_status = 200
+    def check_results(self, urls: list[str], expected_status: int) -> None:
+        """Tests the given URL's against the expected status code"""
         for url in urls:
             with self.subTest(url=url):
                 status = self.get_mini_url(url)
                 self.assertEqual(expected_status, status)
 
+    def test_valid_url_protocols(self) -> None:
+        """Tests URL's with valid protocols"""
+        urls = ['https://github.com', 'http://google.com']
+        expected_status = 200
+        self.check_results(urls, expected_status)
+
     def test_invalid_url_protocols(self) -> None:
+        """Tests URL's with invalid protocols"""
         urls = [
             'htp://google.com', 'ssh://google.com', 'google.com',
             'http//google.com', 'https:/google.com', r'https:\\google.com'
         ]
         expected_status = 422
-        for url in urls:
-            with self.subTest(url=url):
-                status = self.get_mini_url(url)
-                self.assertEqual(expected_status, status)
+        self.check_results(urls, expected_status)
+
+    def test_invalid_authority(self) -> None:
+        """Tests URL's with invalid authorities"""
+        urls = [
+            'https:///google.com', 'https://user:password@google.com',
+            'http://google/notallowed.com', 'http://google@notallowed.com',
+            'http://google?notallowed.com', 'http://google#notallowed.com'
+        ]
+        expected_status = 422
+        self.check_results(urls, expected_status)
 
 
 if __name__ == '__main__':
