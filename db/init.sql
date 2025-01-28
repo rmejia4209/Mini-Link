9af5e01db4c03a9c33f7f6d10722d4d7bcf09d1d
Rigo Mejia <rmejia4209@gmail.com>
2025-01-28 17:27:34 -0600

    Created script to create a database

---
+++ db/init.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE IF NOT EXISTS mini_link_db;
+USE mini_link_db;
+
+/*
+ * Delete tables if they exist.
+ */
+SET FOREIGN_KEY_CHECKS = 0;
+SET FOREIGN_KEY_CHECKS = 1;
+
+-- Create mini links table
+CREATE TABLE mini_links (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    url VARCHAR(1024) NOT NULL,
+    alias VARCHAR(10) NOT NULL,
+    expiration DATETIME NOT NULL,
+    visits INT DEFAULT 0
+);
