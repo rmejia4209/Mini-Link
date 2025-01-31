c3ec410754b384988c4a3c809fdab9535707788b
Rigo Mejia <rmejia4209@gmail.com>
2025-01-31 15:07:46 -0600

    Added query to return last ID used in mini_links table

--- api/db/queries.py
+++ api/db/queries.py
@@ -2,11 +2,11 @@ import os
 import dotenv
 from typing import Annotated
 from fastapi import Depends
-from sqlmodel import create_engine, Session, select
+from sqlmodel import create_engine, Session, select, desc
 from .models import MiniLinks
 
 dotenv.load_dotenv()
-engine = create_engine(os.getenv('DB_URL'), echo=True)
+engine = create_engine(os.getenv('DB_URL'), echo=int(os.getenv('VERBOSE')))
 
 
 def get_session():
@@ -22,3 +22,8 @@ def get_mini_link_details(
 ) -> MiniLinks | None:
     query = select(MiniLinks).where(MiniLinks.alias == mini_link)
     return session.exec(query).first()
+
+
+def get_last_mini_link_id(session: SessionDep) -> int | None:
+    query = select(MiniLinks.id).order_by(desc(MiniLinks.id)).limit(1)
+    return session.exec(query).all()
