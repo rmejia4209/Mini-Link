f3dc18567f6d0ed748e73413e6ce6e751452ba53
Rigo Mejia <rmejia4209@gmail.com>
2025-01-31 20:31:47 -0600

    Added queries to add minilinks with unique alias

--- api/db/queries.py
+++ api/db/queries.py
@@ -3,7 +3,9 @@ import dotenv
 from typing import Annotated
 from fastapi import Depends
 from sqlmodel import create_engine, Session, select, desc
-from .models import MiniLinks
+from ..models import MiniLinkCreate
+from .models import MiniLink
+from ..utils import generate_alias
 
 dotenv.load_dotenv()
 engine = create_engine(os.getenv('DB_URL'), echo=int(os.getenv('VERBOSE')))
@@ -17,13 +19,49 @@ def get_session():
 SessionDep = Annotated[Session, Depends(get_session)]
 
 
-def get_mini_link_details(
-    mini_link: str, session: SessionDep
-) -> MiniLinks | None:
-    query = select(MiniLinks).where(MiniLinks.alias == mini_link)
+def get_mini_link_details(alias: str, session: SessionDep) -> MiniLink | None:
+    query = select(MiniLink).where(MiniLink.alias == alias)
     return session.exec(query).first()
 
 
-def get_last_mini_link_id(session: SessionDep) -> int | None:
-    query = select(MiniLinks.id).order_by(desc(MiniLinks.id)).limit(1)
-    return session.exec(query).all()
+def find_match(
+    mini_link: MiniLinkCreate, session: Session
+) -> int | None:
+    query = select(MiniLink.id).where(MiniLink.alias == mini_link.alias)
+    match = session.exec(query).first()
+    return match if match else None
+
+
+def add_alias(mini_link: MiniLinkCreate, session: Session) -> MiniLinkCreate:
+    query = select(MiniLink.id).order_by(desc(MiniLink.id)).limit(1)
+    _id = session.exec(query).all()
+    _id = 52**2 if not _id else (_id[0] + 1)
+    mini_link.alias = generate_alias(_id)
+    match = find_match(mini_link, session)
+
+    while match:
+        mini_link.alias = generate_alias(match)
+        match = find_match(mini_link, session)
+    return mini_link
+
+
+def add_or_verify(
+    mini_link: MiniLinkCreate, session: SessionDep
+) -> MiniLinkCreate | None:
+    if mini_link.alias:
+        match_found = bool(find_match(mini_link, session))
+        return None if match_found else mini_link
+    return add_alias(mini_link, session)
+
+
+def add_mini_link(
+    mini_link: Annotated[MiniLinkCreate | None, Depends(add_or_verify)],
+    session: SessionDep
+) -> MiniLink | None:
+    if not mini_link:
+        return mini_link
+    db_mini_link = MiniLink.model_validate(mini_link)
+    session.add(db_mini_link)
+    session.commit()
+    session.refresh(db_mini_link)
+    return db_mini_link
